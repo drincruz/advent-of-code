024a987e2b009e631af2a60fca0583f610a3a628
Adrian Cruz <drincruz@gmail.com>
2020-12-03 23:57:17 -0500

    [Ruby] 2020 Day03 - Part 2
    Refactor Day03 - Part1 to have multiple steps right and multiple steps down.

--- 2020/ruby/day03.rb
+++ 2020/ruby/day03.rb
@@ -6,29 +6,35 @@ def read_file(input_file)
   file.readlines.map(&:chomp)
 end
 
-def traverse(row, current, count)
+def traverse(row, current, step, count)
   # Sample rows
   # .........#..##.##..............
   # #...#.#..#.....................
   count += 1 if row[current] == '#'
-  current += 3
+  current += step
   current -= row.length if current >= row.length
 
   [current, count]
 end
 
-def traverse_map
+def traverse_map(step, row_step)
   tree_map = read_file('day03.txt')
 
   row = 0
   current = 0
   count = 0
   while row < tree_map.length
-    current, count = traverse(tree_map[row], current, count)
-    row += 1
+    current, count = traverse(tree_map[row], current, step, count)
+    row += row_step
   end
 
   count
 end
 
-puts traverse_map
+def main
+  steps = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+
+  steps.map { |step, row_step| traverse_map(step, row_step) }.reduce(1, :*)
+end
+
+puts main
