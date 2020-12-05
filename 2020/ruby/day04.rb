450db99c1a496490e9ddedb805aaaff37370a150
Adrian Cruz <drincruz@gmail.com>
2020-12-05 00:04:24 -0500

    [Ruby] 2020 Day04 - Part 2 (DNF)
    Refactor the code from Part 1 to have more validation.
    Getting kinda tired, so I am committing what I have.

--- 2020/ruby/day04.rb
+++ 2020/ruby/day04.rb
@@ -12,7 +12,7 @@ def update_passport(data, key, val)
   data
 end
 
-def valid?(passport_record)
+def valid_keys?(passport_record)
   # byr (Birth Year)
   # cid (Country ID) *optional*
   # ecl (Eye Color)
@@ -26,6 +26,59 @@ def valid?(passport_record)
   required.all? { |key| passport_record.keys.include?(key) }
 end
 
+def valid_data?(passport_record)
+  # byr (Birth Year) - four digits; at least 1920 and at most 2002.
+  # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+  # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+  # hgt (Height) - a number followed by either cm or in:
+  # If cm, the number must be at least 150 and at most 193.
+  # If in, the number must be at least 59 and at most 76.
+  # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+  # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+  # pid (Passport ID) - a nine-digit number, including leading zeroes.
+  # cid (Country ID) - ignored, missing or not.
+  byr = passport_record.fetch("byr", 0).to_i
+  return false if 1920 > byr || byr > 2002
+
+  iyr = passport_record.fetch("iyr", 0).to_i
+  return false if 2010 > iyr || iyr > 2020
+
+  eyr = passport_record.fetch("eyr", 0).to_i
+  return false if 2020 > eyr || eyr > 2030
+
+  hgt = passport_record.fetch("hgt", false)
+  return false unless hgt.end_with?('cm') || hgt.end_with?('in')
+
+  if hgt.end_with?('cm')
+    cm = hgt.sub!('cm', '').to_i
+    return false if 150 > cm || cm > 193
+  else
+    inches = hgt.sub!('inches', '').to_i
+    return false if 59 > inches || inches > 76
+  end
+
+  hcl = passport_record.fetch("hcl", false)
+  return false unless hcl.start_with?('#')
+
+  hcl.sub!('#', '')
+  return false unless hcl.length == 6
+
+  valid_chars = {}
+  (0..9).each { |n| valid_chars[n.to_s] = 1 }
+  ('a'..'f').each { |ch| valid_chars[ch] = 1 }
+  return false unless hcl.split('').all? { |ch| valid_chars.fetch(ch, false) }
+
+  # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+  ecl = passport_record.fetch("ecl", false)
+  return false unless %w[amb blu brn gry grn hzl oth].include?(ecl)
+
+  pid = passport_record.fetch("pid", false)
+  return false unless pid.length == 9
+
+  nums = (0..9).to_a.map { |n| n.to_s }
+  pid.split('').all? { |n| nums.include?(n) }
+end
+
 def parse_line(line)
   line.split(' ').map { |data| data.split(':') }
 end
@@ -37,7 +90,7 @@ def read_passport
   count = 0
   data.each do |line|
     if line.length.zero?
-      count += 1 if valid?(passport)
+      count += 1 if valid_keys?(passport) && valid_data?(passport)
       passport = {}
     end
 
@@ -46,7 +99,7 @@ def read_passport
   end
 
   # We need to check the last passport record.
-  valid?(passport) ? count + 1 : count
+  valid_keys?(passport) && valid_data?(passport) ? count + 1 : count
 end
 
 puts read_passport
