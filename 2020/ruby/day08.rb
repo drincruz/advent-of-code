521f71ad76d7157e4056509de5b0ddb46159310e
Adrian Cruz <drincruz@gmail.com>
2020-12-08 23:30:42 -0500

    [Ruby] 2020 Day08 - Part 2
    Refactor Part 1 to find an incorrect jmp or nop so that the sequence completes successfully and return
    the accumulation value.

--- 2020/ruby/day08.rb
+++ 2020/ruby/day08.rb
@@ -1,5 +1,9 @@
 # 2020: Day08
 # Part 1
+# Find the loop in the operations and return the accumulation value.
+# Part 2
+# Refactor Part 1 to find the incorrect nop or jmp so that the sequence
+# completes successfully. Return the accumulation value.
 
 require_relative './utils'
 require 'set'
@@ -18,14 +22,14 @@ class Day08
     [op, num.to_i]
   end
 
-  def process_operations
-    operations = read_file('day08.txt').map { |line| parse_line(line) }
-    accumulation = 0
+  def process_operations(operations)
     seen_operations = Set.new
     operation_index = 0
+    accumulation = 0
 
-    Kernel.loop do
-      return accumulation if seen_operations.include?(operation_index)
+    while operation_index < operations.length
+      # if we are looping
+      return nil if seen_operations.include?(operation_index)
 
       op, num = operations[operation_index]
       seen_operations.add(operation_index)
@@ -39,7 +43,42 @@ class Day08
         operation_index += 1
       end
     end
+
+    accumulation
+  end
+
+  def main
+    operations = read_file('day08.txt').map { |line| parse_line(line) }
+    i = 0
+
+    while i < operations.length
+      op, num = operations[i]
+
+      if op == 'jmp'
+        operations[i] = ['nop', num]
+      elsif op == 'nop'
+        operations[i] = ['jmp', num]
+      else
+        i += 1
+        next
+      end
+
+      accumulation = process_operations(operations)
+
+      if accumulation.nil?
+        if op == 'jmp'
+          operations[i] = ['jmp', num]
+        elsif op == 'nop'
+          operations[i] = ['nop', num]
+        end
+
+        i += 1
+        next
+      end
+
+      return accumulation
+    end
   end
 end
 
-puts Day08.new.process_operations
+puts Day08.new.main
