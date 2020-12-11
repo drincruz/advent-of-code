546005523004b3f76bfd3d2caee5e34d221c9349
Adrian Cruz <drincruz@gmail.com>
2020-12-11 17:17:35 -0500

    DNF Day10 Part 2
    did not finish, still WIP

--- 2020/ruby/day10.rb
+++ 2020/ruby/day10.rb
@@ -2,6 +2,9 @@
 # Part 1
 # Sort an array of integers, find the counts of differences of 1 and 3 within
 # the array. Take those differences and find a product.
+# Part 2
+# Using the sorted array of integers, now find all combinations of the array
+# that still support the rules of an integers being within +3.
 
 require_relative './utils'
 require 'set'
@@ -10,13 +13,17 @@ require 'set'
 class Day10
   include Utils
 
-  def main
+  def jolts
     jolts = read_file('day10.txt').map(&:to_i).sort
     # We are pre-pending a 0 to represent the outlet.
     jolts.unshift(0)
     # We will push the max value + 3 to the end to represent the device.
     jolts.push(jolts[-1] + 3)
 
+    jolts
+  end
+
+  def main
     one_jolt_differences = 0
     three_jolt_differences = 0
     left = 0
@@ -32,6 +39,22 @@ class Day10
 
     puts one_jolt_differences * three_jolt_differences
   end
+
+  def fn
+    @combinations = []
+    (0..jolts.length).each { |n| backtrack(n, [0]) }
+    puts @combinations
+  end
+
+  def backtrack(index, tmp_arr)
+    @combinations.push(tmp_arr) if index == jolts.length
+
+    (index..jolts.length - 1).each do |i|
+      tmp_arr.push(tmp_arr[i])
+      backtrack(index + 1, tmp_arr + [jolts[i]])
+      tmp_arr.shift
+    end
+  end
 end
 
-Day10.new.main
+Day10.new.fn
