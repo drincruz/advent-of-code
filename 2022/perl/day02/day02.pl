e817dbab4ed97811d03e51326d013d7e5051e78c
Adrian Cruz <drincruz@gmail.com>
2022-12-13 20:46:31 -0500

    [Perl] 2022 Day02, Part02
     - Update for Part02
    
        * X, Y, Z are actually supposed to be Lose, Draw, Win respectively.
        * So now, we want to choose based off of the outcomes of XYZ.

--- 2022/perl/day02/day02.pl
+++ 2022/perl/day02/day02.pl
@@ -1,5 +1,6 @@
 #!/usr/local/bin/perl
 
+## Part 01
 # A: Rock, B: Paper, C: Scissors
 # X: Rock, Y: Paper, Z: Scissors
 
@@ -10,12 +11,12 @@
 
 # Rock 1, Paper 2, Scissors 3
 my %hand_shapes = (
-  "A" => 1,
-  "B" => 2,
-  "C" => 3,
-  "X" => 1,
-  "Y" => 2,
-  "Z" => 3
+    "A" => 1,
+    "B" => 2,
+    "C" => 3,
+    "X" => 1,
+    "Y" => 2,
+    "Z" => 3
 );
 
 ## Losses
@@ -32,15 +33,33 @@ my %hand_shapes = (
 # C Z (Scissors, Scissors)
 
 my %scenarios = (
-  "A X" => 3,
-  "A Y" => 6,
-  "A Z" => 0,
-  "B X" => 0,
-  "B Y" => 3,
-  "B Z" => 6,
-  "C X" => 6,
-  "C Y" => 0,
-  "C Z" => 3,
+    "A X" => 3,
+    "A Y" => 6,
+    "A Z" => 0,
+    "B X" => 0,
+    "B Y" => 3,
+    "B Z" => 6,
+    "C X" => 6,
+    "C Y" => 0,
+    "C Z" => 3,
+);
+
+## Part02
+# X: I should lose
+# Y: I should draw
+# Z: I should win
+my %outcomes = (
+    "X" => 0,
+    "Y" => 3,
+    "Z" => 6,
+);
+
+# What we _should_ choose based off of the outcome
+# fetch a nested hash which is a hash of { opponent => my_choice }
+my %choices = (
+    "X" => {"A" => "C", "B" => "A", "C" => "B"},
+    "Y" => {"A" => "A", "B" => "B", "C" => "C"},
+    "Z" => {"A" => "B", "B" => "C", "C" => "A"},
 );
 
 ## From ChatGPT :)
@@ -49,16 +68,21 @@ open(my $fh, '<', 'data.txt') or die "Could not open file: $!";
 
 # Initialize total score
 $total_score = 0;
+# Keeping this separate for Part02
+$total_score2 = 0;
 
 # Read the file line by line
 while (my $line = <$fh>) {
-  $line =~ s/^\s+|\s+$//g ;
-  my @round = split(' ', $line);
+    $line =~ s/^\s+|\s+$//g ;
+    my @round = split(' ', $line);
 
-  $total_score += ($scenarios{$line} + $hand_shapes{@round[1]});
+    $total_score += ($scenarios{$line} + $hand_shapes{@round[1]});
+    my $choice = $choices{@round[1]}{@round[0]};
+    $total_score2 += ($outcomes{@round[1]} + $hand_shapes{$choice});
 }
 
 print "$total_score\n";
+print "$total_score2\n";
 
 # Close the file
 close($fh);
